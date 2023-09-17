e345c0a87e3cb2bf48528672962512195177d8ee
axiaole <2267508839@qq.com>
2023-09-17 21:34:59 +0800

    小乐博客配置信息

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi, 我是一个打工人, Hi, 我是一个干饭人, 业余程序员,欢迎来到我的博客🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi, 我是一个打工人, Hi, 我是一个干饭人, 业余程序员, 欢迎来到我的博客🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小乐', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '小乐的笔记本，记录小乐学习和成长，也希望以此结实更多的前辈和好友。分享小乐在学习、编程、探索、研究等...各个领域的点点滴滴，希望与你共同进步、共同成长。', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Xiaole Blog', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '生命不息，折腾不止。\n 编程爱好者 \n 特效爱好者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://xiaole.me', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小乐博客,小乐的秘密基地,小乐的笔记本,小乐学习和成长,代码,编程,探索,研究,折腾', // 网站关键词 英文逗号隔开
 
